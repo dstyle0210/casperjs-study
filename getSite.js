b005f3103f44e460ac2acd62415019029edfb364
dstyle0210 <dstyle0210@gmail.com>
2017-06-30 18:08:06 +0900

    init

--- getSite.js
+++ getSite.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var _ = require("lodash");
 var open = require("open");
 var webdriver = require('selenium-webdriver'), By = webdriver.By, until = webdriver.until;
 var driver = new webdriver.Builder().forBrowser('chrome').build();
@@ -9,13 +11,6 @@ var EastBay = require("./SneakerReleaseCalendar/getEastBay.js");
 var FinishLine = require("./SneakerReleaseCalendar/getFinishLine.js");
 var FootAction = require("./SneakerReleaseCalendar/getFootAction.js");
 var FootLocker = require("./SneakerReleaseCalendar/getFootLocker.js");
-
-
-Nike.get(driver,function(){
-    driver.quit();
-});
-
-/*
 
 NikeKorea.get(driver,function() {
     console.log("나이키 코리아 완료.");
@@ -31,17 +26,32 @@ NikeKorea.get(driver,function() {
                         console.log("풋락커 완료.");
                         Ruvilla.get(driver,function() {
                             console.log("루빌라 완료.");
-                            driver.quit();
-                            open("http://localhost:63342/js-kids/releaseCalendar.html");
-                        });
+                            Nike.get(driver,function(){
+                                console.log("나이키영문 완료.");
 
+                                driver.get("http://localhost:63342/js-kids/releaseCalendar.html");
+                                driver.sleep(2000);
+                                driver.wait(function(){
+                                    return driver.executeScript(function(){
+                                        return setHTML();
+                                    });
+                                },500).then(function(html){
+                                    var comingsoon = '<!doctype html><html lang="ko"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link rel="stylesheet" href="style.css"><title>곧 발매될 신발목록 : Coming Soon</title></head><body>';
+                                    comingsoon += "<h1>Take My Money!</h1>";
+                                    comingsoon += html;
+                                    comingsoon += "</body></html>";
+                                    fs.writeFileSync("../comingsoon/index.html",comingsoon);
+                                });
+                                driver.quit();
+
+                            });
+                        });
                     });
                 });
             });
         });
     });
 });
-*/
 
 /*
 NikeKorea.get(function(){
