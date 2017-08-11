198d3ec8280e831b494818c002ad99c551769928
dstyle0210 <dstyle0210@gmail.com>
2017-08-11 15:13:26 +0900

    stock commit

--- stock.js
+++ stock.js
@@ -7,9 +7,12 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 var DB = [];
 var DB2 = [];
+var DB3 = [];
+var FINAL = [];
 getData(1);
-function getData(idx){
-    var url = "http://finance.naver.com/sise/dividend_list.nhn?field=dividend_rate&sosok=&ordering=desc&page="+idx;
+function getData(page){
+    console.log("배당주목록 수집중 : page"+page);
+    var url = "http://finance.naver.com/sise/dividend_list.nhn?field=dividend_rate&sosok=&ordering=desc&page="+page;
     driver.get(url);
     driver.executeScript(function() {
         var scriptElt = document.createElement('script');
@@ -30,8 +33,13 @@ function getData(idx){
                                 if( 3 < (tds.eq(6).text()*1) ) { // ROE가 3보다 커야 한다.
                                     if( ((tds.eq(11).text()*1) <= (tds.eq(10).text()*1)) && ((tds.eq(10).text()*1) <= (tds.eq(9).text()*1)) ){ // 3년전 배당보다, 2년전이 크거나 같고, 2년전은 1년전 보다 크거나 같아야 한다.
                                         data.push({
-                                            name:tds.eq(0).text(),
-                                            link:"http://finance.naver.com"+tds.eq(0).find("a").attr("href")
+                                            name:tds.eq(0).text(), // 주식명
+                                            price:tds.eq(1).text(), // 현재주가
+                                            link:"http://finance.naver.com"+tds.eq(0).find("a").attr("href"), // 상세링크
+                                            refundRate:tds.eq(4).text(), // 배당률(작년배당금 / 현재주가)
+                                            PBR:(tds.eq(8).text()*1),
+                                            ROE:(tds.eq(6).text()*1),
+                                            oldRefund:(tds.eq(9).text()*1) // 작년배당금
                                         });
                                     };
                                 };
@@ -43,26 +51,26 @@ function getData(idx){
             return data;
         });
     }).then(function(sdata){
-        if(idx<3){
-            DB.push(sdata);
-            getData(idx+1);
+        DB.push(sdata);
+        if(page<7){
+            getData(page+1);
         }else{
-            DB2 = _.concat(DB[0],DB[1]);
-            // fs.writeFileSync("./tete2.json",JSON.stringify(DB2) );
+            DB2 = _.concat(DB[0],DB[1],DB[2],DB[3],DB[4],DB[5],DB[6]);
+            // DB2 = _.concat(DB[0],DB[1]);
+            // fs.writeFileSync("./stock_DB2.json",JSON.stringify(DB2) );
             getStockData(0);
         };
     });
 };
 function getStockData(idx){
-    console.log(DB2[idx]+","+idx);
     if(DB2.length==idx){
-        var DB3 = _.filter(DB2,function(item){return item.per});
-        fs.writeFileSync("./tete2.json",JSON.stringify(DB3) );
-        driver.quit();
-        console.log("끝");
+        DB3 = _.filter(DB2,function(item){return item.per});
+        fs.writeFileSync("./stock_DB3.json",JSON.stringify(DB3) );
+        getStockDetailData(0);
+        console.log("주식기본정보 정리 끝");
         return;
     };
-    console.log("("+DB2.length+"/"+(idx+1)+")");
+    console.log("주식기본정보 수집중 : "+DB2[idx].name);
     var url = DB2[idx].link;
     driver.get(url);
     driver.executeScript(function() {
@@ -80,28 +88,67 @@ function getStockData(idx){
             data.per = $("th:contains('EPS(WISEfn)')").next().find("em:eq(0)").text().trim().match(/[0-9\.]/gi).join("") * 1; // 회사 PER
             data.p5 = $("caption:contains('외국인 기관')").parent().find("td:contains('+')").length; // 최근 5일 외국인,기관 매수횟수
             data.m5 = $("caption:contains('외국인 기관')").parent().find("td:contains('-')").length; // 최근 5일 외국인,기관 매도횟수
+            data.dlink = "http://finance.naver.com"+$("a:contains('투자자별 매매동향')").attr("href"); // 투자자 정보
+            data.preRefund = $("strong:contains('주당배당금(원)')").parent().parent().find("td:eq(3)").text().trim(); // 예상배당금
+            if( data.preRefund!="" && data.preRefund!="-" ){
+                data.preRefund = data.preRefund.match(/[0-9\.]/gi).join("") * 1;
+            };
             return data;
         });
+        //   $("strong:contains('주당배당금(원)')").parent().parent().find("td:eq(3)").text().trim().match(/[0-9\.]/gi).join("") * 1;
     }).then(function(data){
         if(10 < data.frRate){ // 외국인 취득률이 10%가 넘어야 한다.
             if(data.per < data.midPer){ // 업계평균 PER 보다 회사PER이 작아야 한다.
-                if(data.p5 <= data.m5){ // 최근 5일 외국인,기관 매수가 매도보다 같거나 많아야 한다.
+                if(data.m5 <= data.p5){ // 최근 5일 외국인,기관 매수가 매도보다 같거나 많아야 한다.
                     DB2[idx].frRate = data.frRate;
                     DB2[idx].plus = data.p5;
                     DB2[idx].per = data.per;
-                }else{
-                    DB2[idx].del = "true";
+                    DB2[idx].dlink = data.dlink;
+                    DB2[idx].preRefund = data.preRefund;
                 };
-            }else{
-                DB2[idx].del = "true";
             };
-        }else{
-            DB2[idx].del = "true";
         };
         getStockData(idx+1);
         driver.quit();
     });
 };
+
+
+function getStockDetailData(idx){
+    if(DB3.length==idx){
+        FINAL = _.filter(DB3,function(item){return item.pass});
+        // fs.writeFileSync("./stock.json",JSON.stringify(FINAL) );
+        console.log("주식상세정보 정리 끝");
+        driver.quit();
+        return;
+    };
+    console.log("주식상세정보 수집중 : "+DB3[idx].name);
+    var url = DB3[idx].dlink;
+    driver.get(url);
+    driver.executeScript(function() {
+        var scriptElt = document.createElement('script');
+        scriptElt.type = 'text/javascript';
+        scriptElt.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
+        document.getElementsByTagName('head')[0].appendChild(scriptElt);
+    });
+    driver.sleep(1000);
+    driver.wait(function(){
+        return driver.executeScript(function() {
+            var data = {};
+            data.now = $(".tlline2 + table").find("tr:eq(3) td:last-child").text().match(/[0-9\.]/gi).join("") * 1;
+            data.old = $(".tlline2 + table").find("tr:eq(31) td:last-child").text().match(/[0-9\.]/gi).join("") * 1;
+
+            return data;
+        });
+        //
+    }).then(function(data){
+        if(data.old<data.now){ // 외국인 취득률이 20일전 보다 많아졌다.
+            DB3[idx].pass = "true";
+        };
+        getStockDetailData(idx+1);
+    });
+}
+
 
 // $("strong:contains('외국인소진율(B/A)')").css({"border":"1px solid red"});
 
