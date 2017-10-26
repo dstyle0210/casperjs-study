b449a3c48b4cebbc9ba86a606595bd250060f141
dstyle0210 <dstyle0210@gmail.com>
2017-10-26 10:49:11 +0900

    init

---
+++ selenium/nike2.js
@@ -0,0 +1,66 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+var url = "http://www.nike.co.kr/snkrs/detail.lecs?displayNo=NK1A86A51";
+driver.get(url);
+driver.findElement(By.css('.global_login a')).click();
+driver.wait(function(){
+    return until.elementLocated(By.name('loginId'));
+},1000);
+driver.findElement(By.name('loginId')).sendKeys('blackrabit'); // 수정필요.
+driver.findElement(By.name('password')).sendKeys('bobby2'); // 수정필요.
+driver.findElement(By.css('.global_id_pass_info a')).click();
+driver.sleep(500);
+driver.wait(function(){
+    return driver.executeScript(function(){
+        return $(".global_login_window > a").text().trim()=="원용봉님"; // 수정필요.
+    });
+},500).then(function(){
+    driver.wait(function(){
+        return driver.executeScript(function(){
+            return ($("#sizeArea").length) ? "1" : "0";
+        });
+    },500).then(function(area){
+        areaCheck(area);
+    });
+});
+
+function areaCheck(area){
+    if(area=="1"){
+        // 있다.
+        driver.executeScript(function() {
+            $("#sizeArea a[data-optionvaluecode=275]").click();
+            $(".cart-area a").click();
+        }).then(function(){
+            driver.sleep(1000);
+            driver.switchTo().alert().accept();
+            driver.wait(function(){
+                return until.elementLocated(By.id('pay_price_str'));
+            },1000);
+            driver.executeScript(function() {
+                $("#selectcard option:eq(4)").attr("selected", "selected");
+                fn_CreditCardSel();
+                $("#assent").attr("checked","checked");
+                fn_deliveryChkAjax('',3);
+            });
+        });
+    }else{
+        // 없다.
+        driver.sleep(100);
+        reCheck();
+    };
+}
+
+function reCheck(){
+    console.log("recheck");
+    driver.get(url);
+    driver.executeScript(function() {
+        return ($("#sizeArea").length) ? "1" : "0";
+    }).then(function(area){
+        areaCheck(area);
+    });
+};
